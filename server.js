987a96b3f6e33a21ce577a60a452ba9be485d8c4
PriyanshuQ <80645068+PriyanshuQ@users.noreply.github.com>
2023-06-18 00:50:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const cors = require('cors');
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
 const connectDatabase = require("./config/database");
-
+const path = require("path");
 // connecting to database
 connectDatabase();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 const port = process.env.PORT || 5000;
 
 // import routes
-const Routes = require("./routes/Route")
-app.use('/', Routes);
+const Routes = require("./routes/Route");
+app.use("/", Routes);
+app.use("*", function (req, res) {
+  res.sendfile(path.join(__dirname, "./client/build/index.html"));
+});
 
-
-app.listen(port, () => console.log(`Node server started at port ${port}`))
+app.listen(port, () => console.log(`Node server started at port ${port}`));
