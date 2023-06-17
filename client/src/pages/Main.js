e26bed8d10b969faf914d1c324a25c8284132b78
PriyanshuQ <80645068+PriyanshuQ@users.noreply.github.com>
2023-06-18 00:12:34 +0530

    Final_Code

---
+++ client/src/pages/Main.js
@@ -0,0 +1,97 @@
+import { Box, styled, MenuItem, MenuList, Grid } from "@mui/material";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import ProgramCore from "./ProgramCore";
+import ProrgamElective from "./ProrgamElective";
+import OpenElective from "./OpenElective";
+import NaturalScience from "./NaturalScience";
+const Container = styled(Box)`
+  display: flex;
+  min-height: 100vh;
+  width: 100%;
+`;
+
+const Left = styled(Box)`
+  background: #884a39;
+  color: #f9e0bb;
+  flex: 1;
+  padding: 10px 10px;
+`;
+const Right = styled(Grid)`
+  background: #ecf8f9;
+  color: #e55807;
+  flex: 4;
+  padding: 10px 10px;
+  overflow: scroll;
+  overflow-x: hidden;
+  height: 100vh;
+`;
+
+const Ml = styled(MenuList)``;
+
+const Mi = styled(MenuItem)`
+  font-weight: 700;
+  margin-bottom: 30px;
+`;
+
+const Main = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <Container>
+        <Left>
+          <Ml>
+            <Link
+              to={"/programcore"}
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <Mi>Program Core</Mi>
+            </Link>
+            <Link
+              to={"/prorgamelective"}
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <Mi>Program Elective</Mi>
+            </Link>
+            <Link
+              to={"/naturalscience"}
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <Mi>Natural Science, Humanities and Management</Mi>
+            </Link>
+            <Link
+              to={"/openelective"}
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <Mi>Open Elective</Mi>
+            </Link>
+            <div
+              onClick={() => {
+                localStorage.clear();
+                navigate("/login");
+              }}
+            >
+              <Link
+                to={"/logout"}
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <Mi>Log Out</Mi>
+              </Link>
+            </div>
+          </Ml>
+        </Left>
+        <Right>
+          <Grid item lg={6} md={6} xs={12}>
+            <Routes>
+              <Route path="/programcore" element={<ProgramCore />} />
+              <Route path="/prorgamelective" element={<ProrgamElective />} />
+              <Route path="/naturalscience" element={<NaturalScience />} />
+              <Route path="/openelective" element={<OpenElective />} />
+            </Routes>
+          </Grid>
+        </Right>
+      </Container>
+    </>
+  );
+};
+
+export default Main;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const cors = require('cors');
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
 const connectDatabase = require("./config/database");
+const path = require("path");
 
 // connecting to database
 connectDatabase();
@@ -10,12 +11,16 @@ connectDatabase();
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 const port = process.env.PORT || 5000;
 
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 // import routes
-const Routes = require("./routes/Route")
-app.use('/', Routes);
+const Routes = require("./routes/Route");
+app.use("/", Routes);
 
-
-app.listen(port, () => console.log(`Node server started at port ${port}`))
+app.listen(port, () => console.log(`Node server started at port ${port}`));
